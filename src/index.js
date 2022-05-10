fec246d37bc72b5e15681dbacfa918f201f9b9b6
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-05-10 16:11:12 -0500

    create a new game function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+
 import './styles.css';
+
+
+let api = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/"
+let gameid
+ const gamename= async (gameid)=>{
+    fetch(api,{
+    method: "POST",
+    body: JSON.stringify({ "name": "Santiago's Game" })
+    ,
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+    })
+    .then(response=>response.json())
+    .then(data=>{gameid=data.result.slice(14,34)
+    console.log(gameid)
+})
+}
+
+gamename(gameid)
+
+
